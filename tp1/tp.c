1368271245040f9fac31aec5ed34c20ed43e5a09
TicTac <mael.galliot@gmail.com>
2019-10-23 23:10:58 +0200

    Ajout de la fonction display_gdt et explain_gdt

--- tp1/tp.c
+++ tp1/tp.c
@@ -1,9 +1,42 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
 #include <info.h>
+#include <segmem.h>
 
 extern info_t *info;
 
-void tp()
-{
+//Affichage
+#define  base(_dsc)  _dsc.base_3<<24 | _dsc.base_2<<16 | _dsc.base_1
+#define  limit(_dsc) _dsc.limit_2<<16| _dsc.limit_1
+
+/* Affichage de la GDT qui est chargé dans le noyau
+ *
+ */
+void display_gdt(){
+  gdt_reg_t gdtr; //Création du pointeur vers la gdt
+  unsigned int i; //int = long = 4 octets 
+  
+
+  get_gdtr(gdtr);//Récupère la gdt actuel avec instruction asm sgdt
+  unsigned int gdt_length = (gdtr.limit+1)/sizeof(seg_desc_t);//Taille de la gdt
+
+  debug("\n[%s] @GDT = %p - Length = %d \n",__func__,gdtr.desc,gdt_length);
+  debug("|----------------------------------- GDT -----------------------------------|\n");
+  for(i=0; i<gdt_length;i++){ //On boucle pour tous les descripteurs
+           debug("|-Num : %d --- Base : %p --- Limit : 0x%x ---P : 0x%x ---g : 0x%x ----------| <-%p\n",i,base(gdtr.desc[i]),limit(gdtr.desc[i]),gdtr.desc[i].p,gdtr.desc[i].g,&(gdtr.desc[i]));
+    debug("|-Type : %d --- s : 0x%x --- dpl : 0x%x --- avl : 0x%x --- d/b : 0x%x -----------|\n",gdtr.desc[i].type,gdtr.desc[i].s,gdtr.desc[i].dpl,gdtr.desc[i].avl,gdtr.desc[i].d);
+    debug("|---------------------------------------------------------------------------|\n"); 
+  }
+}
+
+void explain_desc_gdt(){
+  debug("\n[%s] EXPLICATION DES DESCRIPTEURS DE SEGMENT \n",__func__);
+  debug("\tNum : Numéro du descripteur\n\tBase : @linéaire sur 32 bits, où débute le segment en mémoire par rapport à la mémoire disponible \n\tLimit : 20  bit, sur la longueur du segment (max 4G si à 0xfffff)\n\tP(Present) : 1 bit, détermine si le segment est présent/chargé/référencé dans la mémoire\n\tDPL(Descriptor Priviliege-Level : 2 bit,  indique le niveau de privilège du segment mémoire noyau ring 0 -> 0\n\tAVL(Available To Software) : 1 bit, dispo pour le logiciel (le proc ne s'en occupe pas)\n\td/b(Default Operand Size) : 1 bit, taillle des instructions et des données manipulées. 1= 32bits 0=16bits(défaut)\n\tG(Granularity) : 1 bit, 0 si la limit est exprimé en octet, 1 en page (de 4Ko)\n\tS : 1 bit, à 0 pour les descripteur de segment (code/data) et à 1 pour les descripteur système(TSS/LDT/Gate)\n\tType : 2 bits, en fonction de S change le type de segment pointé");
+  debug("\nPour plus d'info télécharger AMD64 Architecture Prgrammer's Manual Volume 2 :System Programming (cf p80)\n");
+}
+
+
+void tp(){
+  display_gdt();
+  explain_desc_gdt();
 }
