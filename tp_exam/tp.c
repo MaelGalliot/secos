73e9426ff05bb7bfbc1f58f27c70faf27c233ba1
TicTac <mael.galliot@gmail.com>
2019-11-10 15:42:50 +0100

    Mise en place de la segmentation (repise du tp1)

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -1,28 +1,91 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
 #include <info.h>
+#include <segmem.h>
+#include <string.h>
 
 extern info_t *info;
 
+//Affichage
+#define  base(_dsc)  _dsc.base_3<<24 | _dsc.base_2<<16 | _dsc.base_1
+#define  limit(_dsc) _dsc.limit_2<<16| _dsc.limit_1
 
-/*
- * Tâche de l'utilsiateur 1 
- */ 
-void user1(void){
-  debug("[%s",__func__);
-  while(1);
-}
+//Création du tableau de descritpeur de GDT
+#define LENGTH_GDT 5 
+seg_desc_t GDT[LENGTH_GDT];
 
 /* 
- * Tâche de l'utilisateur 2
+ * Affichage de la GDT qui est chargé dans le noyau
  */
-void user2(void){
-  debug("[%s",__func__);
-  while(1);
+void display_gdt(){
+  gdt_reg_t gdtr; //Création du pointeur vers la gdt
+  unsigned int i; //int = long = 4 octets 
+  get_gdtr(gdtr);//Récupère la gdt actuel avec instruction asm sgdt
+  unsigned int gdt_length = (gdtr.limit+1)/sizeof(seg_desc_t);//Taille de la gdt
+
+  debug("\n[%s] @GDT = %p - Length = %d \n",__func__,gdtr.desc,gdt_length);
+  debug("|----------------------------------- GDT -----------------------------------|\n");
+  for(i=0; i<gdt_length;i++){ //On boucle pour tous les descripteurs
+           debug("|-Num : %d --- Base : %p --- Limit : 0x%x ---P : 0x%x ---g : 0x%x ----------| <-%p\n",i,base(gdtr.desc[i]),limit(gdtr.desc[i]),gdtr.desc[i].p,gdtr.desc[i].g,&(gdtr.desc[i]));
+    debug("|-Type : %d --- s : 0x%x --- dpl : 0x%x --- avl : 0x%x --- d/b : 0x%x -----------|\n",gdtr.desc[i].type,gdtr.desc[i].s,gdtr.desc[i].dpl,gdtr.desc[i].avl,gdtr.desc[i].d);
+    debug("|---------------------------------------------------------------------------|\n"); 
+  }
+}
+
+/*
+ * Explication de chaque champ de la GDT
+ */
+void explain_desc_gdt(){
+  debug("\n[%s] EXPLICATION DES DESCRIPTEURS DE SEGMENT \n",__func__);
+  debug("\tNum : Numéro du descripteur\n\tBase : @linéaire sur 32 bits, où débute le segment en mémoire par rapport à la mémoire disponible \n\tLimit : 20  bit, sur la longueur du segment (max 4G si à 0xfffff)\n\tP(Present) : 1 bit, détermine si le segment est présent/chargé/référencé dans la mémoire\n\tDPL(Descriptor Priviliege-Level : 2 bit,  indique le niveau de privilège du segment mémoire noyau ring 0 -> 0\n\tAVL(Available To Software) : 1 bit, dispo pour le logiciel (le proc ne s'en occupe pas)\n\td/b(Default Operand Size) : 1 bit, taillle des instructions et des données manipulées. 1= 32bits 0=16bits(défaut)\n\tG(Granularity) : 1 bit, 0 si la limit est exprimé en octet, 1 en page (de 4Ko)\n\tS : 1 bit, à 0 pour les descripteur de segment (code/data) et à 1 pour les descripteur système(TSS/LDT/Gate)\n\tType : 2 bits, en fonction de S change le type de segment pointé");
+  debug("\nPour plus d'info télécharger AMD64 Architecture Prgrammer's Manual Volume 2 :System Programming (cf p80)\n");
+}
+
+/*
+ * Initialise la GDT avec un le premier descritpeur de segment NULL
+ */
+void init_gdt(){
+  gdt_reg_t gdtr;   //Création du pointeur de GDT
+  gdtr.desc = GDT;  //Application de ce pointeur
+  gdtr.limit = sizeof(GDT)-1; //Appliation de la limite de la GDT
+  memset((void *)&GDT[0],0,sizeof(seg_desc_t)); //Init du segment 0 à null
+  set_gdtr(gdtr);//Chargement de la gdt
+}
+/*
+ * Ajout un descripteur de segment
+ */
+void add_desc_gdt(int index, uint32_t base, uint32_t limit, unsigned short type, unsigned short privilege){
+  gdt_reg_t gdtr;
+  get_gdtr(gdtr);//Chargement de la gdt
+  gdtr.desc[index].base_1 = base;
+  gdtr.desc[index].base_2 = base >> 16;
+  gdtr.desc[index].base_3 = base >> 24;
+  gdtr.desc[index].limit_1 = limit;
+  gdtr.desc[index].limit_2 = limit >> 16;
+  gdtr.desc[index].type =  type;
+  gdtr.desc[index].s = 1; //Segment de code ou de data
+  gdtr.desc[index].dpl = privilege;
+  gdtr.desc[index].p = 1; //Le descripteur est présent
+  gdtr.desc[index].d = 1; //32bit 
+  gdtr.desc[index].g = 1; //La limit est exprimee en page de 4Ko
 }
 
 
+/*
+ * main
+ */
+void tp(){
+  
+  /* CRÉATION DE LA GDT */
+  explain_desc_gdt();                               //Affichage sympatoch pour comprendre la GDT
+  init_gdt();                                       //Initialisation de la GDT avec le segment 0 - NULL
+  add_desc_gdt(1,0,0xfffff,SEG_DESC_CODE_XR,0);     //Création du segment RING 0 - CODE de 0 à 4G
+  add_desc_gdt(2,0,0xfffff,SEG_DESC_DATA_RW,0);     //Création du segment RING 0 - DATA de 0 à 4G
+  add_desc_gdt(3,0,0xfffff,SEG_DESC_CODE_XR,3); //Création du segment RING 3 - CODE de 0 à 4G
+  add_desc_gdt(4,0,0xfffff,SEG_DESC_DATA_RW,3);     //Création du segment RING 3 - CODE de 0 à 4G
+  display_gdt();                                    //Affichage de la GDT
+  /********** */  
 
-void tp()
-{
+set_es(gdt_krn_seg_sel(3)); //Utilisation de ES abritraire
+
 }
