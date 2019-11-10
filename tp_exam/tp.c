e1a88073ed5c8e3281e8e6791a6c9d5d38237242
TicTac <mael.galliot@gmail.com>
2019-11-10 14:52:41 +0100

    Création des tâches user1/2

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -4,6 +4,25 @@
 
 extern info_t *info;
 
+
+/*
+ * Tâche de l'utilsiateur 1 
+ */ 
+void user1(void){
+  debug("[%s",__func__);
+  while(1);
+}
+
+/* 
+ * Tâche de l'utilisateur 2
+ */
+void user2(void){
+  debug("[%s",__func__);
+  while(1);
+}
+
+
+
 void tp()
 {
 }
